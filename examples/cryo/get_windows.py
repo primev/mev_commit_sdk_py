89d72f3d55a7bd1757d89537e82f7a522f98d59c
Evan K <ekcopersonal@gmail.com>
2024-07-18 15:08:57 -0400

    feat: add bidder deposit/withdraw window lookup

---
+++ examples/cryo/get_windows.py
@@ -0,0 +1,24 @@
+import polars as pl
+from mev_commit_sdk_py.cryo_client import CryoClient
+
+
+client = CryoClient()
+
+# bidder address
+address: str = "0xe51EF1836Dbef052BfFd2eB3Fe1314365d23129d".lower()
+
+# get all the deposits and withdraws for an address
+deposits_df: pl.DataFrame = client.get_window_deposits().filter(
+    pl.col('bidder') == address)
+withdraws_df: pl.DataFrame = client.get_window_withdraws(
+).filter(pl.col('bidder') == address)
+
+# extract dataframe columns to lists
+deposit_windows = deposits_df['windowNumber_f64'].to_list()
+withdraw_windows = withdraws_df['window_f64'].to_list()
+
+# take the set difference between deposit_windows and withdraw_windows
+windows = list(set(deposit_windows) - set(withdraw_windows))
+
+print('windows with funds still locked')
+print(windows)
--- src/mev_commit_sdk_py/cryo_client.py
+++ src/mev_commit_sdk_py/cryo_client.py
@@ -58,3 +58,94 @@ class CryoClient:
                        for col in data_df.columns if col.startswith('event__')}
 
         return data_df.rename(rename_dict)
+
+    def get_l1_window_events(self, block_number: str = ':latest') -> str:
+        """
+        Get the L1 block emitted events:
+
+        event signature:
+        `NewL1Block(
+        uint256 indexed blockNumber,
+        address indexed winner,
+        uint256 indexed window
+        )`
+        """
+
+        data_df: pl.DataFrame = cryo.collect(
+            'logs',
+            rpc=self.rpc,
+            hex=True,
+            blocks=[block_number],
+            inner_request_size=10000,
+            contract=['0x2eEbF31f5c932D51556E70235FB98bB2237d065c'],
+            event_signature='NewL1Block(uint256 indexed blockNumber,address indexed winner,uint256 indexed window)',
+            topic0=[
+                '0x8323d3e5d25db513e1a772870aaa45e9b069a13d49879d72e70638b5c1c18cb7']
+        )
+
+        # rename event columns
+        # Create a dictionary mapping old column names to new column names
+        rename_dict = {col: col.replace('event__', '')
+                       for col in data_df.columns if col.startswith('event__')}
+
+        return data_df.rename(rename_dict).drop('blockNumber_binary', 'blockNumber_string', 'window_binary', 'window_string')
+
+    def get_window_deposits(self, block_number: str = ':latest') -> str:
+        """
+        Get historical bidder deposits into windows.
+
+        event signature:
+        `BidderRegistered(
+        address indexed bidder, 
+        uint256 depositedAmount, 
+        uint256 windowNumber
+        )`
+        """
+        data_df: pl.DataFrame = cryo.collect(
+            'logs',
+            rpc=self.rpc,
+            hex=True,
+            blocks=[block_number],
+            # inner_request_size is the number of logs per request. Sparse logs can have a higher value
+            inner_request_size=10000,
+            # Replace with the correct contract address
+            contract=['0x7ffa86fF89489Bca72Fec2a978e33f9870B2Bd25'],
+            event_signature='BidderRegistered(address indexed bidder, uint256 depositedAmount, uint256 windowNumber)',
+        )
+
+        # rename event columns
+        # Create a dictionary mapping old column names to new column names
+        rename_dict = {col: col.replace('event__', '')
+                       for col in data_df.columns if col.startswith('event__')}
+
+        return data_df.rename(rename_dict)
+
+    def get_window_withdraws(self, block_number: str = ':latest') -> str:
+        """
+        Get historical bidder withdraws from the windows.
+
+        event signature:
+        `BidderWithdrawal(
+        address indexed bidder, 
+        uint256 window, 
+        uint256 amount
+        )`
+        """
+        data_df: pl.DataFrame = cryo.collect(
+            'logs',
+            rpc=self.rpc,
+            hex=True,
+            blocks=[block_number],
+            # inner_request_size is the number of logs per request. Sparse logs can have a higher value
+            inner_request_size=10000,
+            # Replace with the correct contract address
+            contract=['0x7ffa86fF89489Bca72Fec2a978e33f9870B2Bd25'],
+            event_signature='BidderWithdrawal(address indexed bidder, uint256 window, uint256 amount)',
+        )
+
+        # rename event columns
+        # Create a dictionary mapping old column names to new column names
+        rename_dict = {col: col.replace('event__', '')
+                       for col in data_df.columns if col.startswith('event__')}
+
+        return data_df.rename(rename_dict)
