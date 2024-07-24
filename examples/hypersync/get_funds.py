c023c3168c0cab11237f6d9f6b8cd69fa15933c5
Evan K <ekcopersonal@gmail.com>
2024-07-24 14:53:29 -0400

    chore: bump to 1.7

---
+++ examples/hypersync/get_funds.py
@@ -0,0 +1,17 @@
+import asyncio
+import polars as pl
+from mev_commit_sdk_py.hypersync_client import Hypersync
+
+# expand polars df output
+pl.Config.set_fmt_str_lengths(200)
+pl.Config.set_fmt_float("full")
+
+# query window data
+client = Hypersync(url='https://mev-commit.hypersync.xyz')
+
+# commit_stores = asyncio.run(client.get_commit_stores())
+# print(commit_stores)
+
+encrypted_stores = asyncio.run(client.get_encrypted_commit_stores())
+print(encrypted_stores)
+
